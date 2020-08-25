997bce936593cbdacc7b9e83c660a011d0acada8
judgedreadnaught <akhanal977@gmail.com>
2020-08-25 15:25:11 -0400

    Finished all four methods that I wanted to. Created the stock class that contains a stock's volume. Also figured how to calculate the mean and standard deviation of a set of numbers. Program seems to be working but will test it on the date that kodak rocketed

--- Unusual Volume/src/Driver.java
+++ Unusual Volume/src/Driver.java
@@ -6,44 +6,28 @@ import java.util.Scanner;
 public class Driver {
 	
 	public static void main(String[] args) {
-		
 		Scanner keyboard = new Scanner(System.in);
 		System.out.println("Please type in the name of the stock ticker: ");
 		String ticker = keyboard.nextLine().toUpperCase();
 		System.out.println("TICKER IS: " + ticker + "\n");
+		Stock stock = new Stock(ticker);
+
 		
-		initialVol(ticker);
-		//Document website;
-		/*try {
-			website = Jsoup.connect("https://finance.yahoo.com/quote/" + ticker +
-					"/history?p=" + ticker).get();
-			Integer volData = 80; // this is the data-reactid number that gives the volume of the stock, increases by 15 for each previous date's volume 
-			Integer dateData = 53;
-			Elements vol = website.getElementsByAttributeValue("data-reactid", volData.toString());
-			Elements date = website.getElementsByAttributeValue("data-reactid", dateData.toString());
-			System.out.println(date.text() + ", the current volume is: " + vol.text() + ".");
-		} catch (Exception e) {
-			System.err.close();
-		}*/
-	}
-	
-	
-	public static void initialVol(String tick) {
-		try {
-			Document website = Jsoup.connect("https://finance.yahoo.com/quote/" + tick +
-					"/history?p=" + tick).get();
-			Integer volData = 80; // this is the data-reactid number that gives the volume of the stock, increases by 15 for each previous date's volume 
-			Integer dateData = 53;
-			Elements vol = website.getElementsByAttributeValue("data-reactid", volData.toString());
-			Elements date = website.getElementsByAttributeValue("data-reactid", dateData.toString());
-			System.out.println(date.text() + ", the current volume is: " + vol.text() + ".");
-		} catch (Exception e) {
-			System.err.close();
+		stock.initialVol();
+		stock.lastVol();
+		boolean x = stock.calculator();
+		if (x) {
+			System.out.println("\ntrue");
+		} else {
+			System.out.println("\nfalse");
 		}
-	}
-	// method that gives the initial volume and date 
 	
-	// method that prints out the volumes in the last 10 days 
+		
+	}
+	
+	// method that gives the initial volume and date  ---- FINISHED 
+	
+	// method that prints out the volumes in the last 5 days ---- FINISHED 
 	
 	// method that calculates the mean and the standard deviation
 	
--- Unusual Volume/src/Stock.java
+++ Unusual Volume/src/Stock.java
@@ -1,4 +1,78 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.jsoup.nodes.Element;
 
 public class Stock {
+	
+	private Integer volData;
+	private Document website;
+	private Integer volume;
+	private Integer dateData;
+	private Double[] volumes;
+	
+	public Stock(String tick) {
+		volData = 0;
+		volume = 0;
+		dateData = 0;
+		volumes = null;
+		try {
+		website = Jsoup.connect("https://finance.yahoo.com/quote/" + tick +
+				"/history?p=" + tick).get();
+		} catch (Exception e) {
+			System.err.close();
+		}
+	}
+	
+	public void initialVol() {
+		try {
+			volData = 65; // this is the data-reactid number that gives the volume of the stock, increases by 15 for each previous date's volume 
+			dateData = 52;
+			Elements vol = website.getElementsByAttributeValue("data-reactid", volData.toString());
+			Elements date = website.getElementsByAttributeValue("data-reactid", dateData.toString());
+			System.out.println(date.text() + "the current volume is: " + vol.text() + ".");
+			volume = Integer.parseInt(vol.text().replaceAll(",", "")); // volume of the stock 
+		} catch (Exception e) {
+			System.err.close();
+		}
+	}
+	
+	// method that prints out the volumes in the last 5 days 
 
+	public void lastVol() {
+		int counter = 0;
+		volumes = new Double[5];
+		System.out.println("Volume in the past 5 days");
+		while (counter < 5) { 
+			volData += 15;
+			Elements vol = website.getElementsByAttributeValue("data-reactid", volData.toString());
+			System.out.println(vol.text());
+			volumes[counter] = Double.parseDouble(vol.text().replaceAll(",", ""));
+			counter++;
+		}
+	}
+	
+	public boolean calculator() {
+		// finding the mean // 349.4
+		//volumes = new Double[] {1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0};
+		double mean = 0;
+		double sd = 0; // standard deviation
+		for (double num : volumes) {
+			mean += num;
+		}
+		mean = mean / volumes.length;
+		for (double num : volumes) {
+			sd += Math.pow(num - mean,  2);
+		}
+		sd = Math.sqrt(sd / volumes.length);
+		System.out.printf("Mean is: %.3f\n",mean);
+		System.out.printf("SD is: %.6f ",sd);
+		
+		double x = (10 * sd) + mean;
+		if (x <= volume) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
